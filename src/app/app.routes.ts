8458264d05266071b6c25a576b2581199b30b585
Ahmad Habiburrahman <ahmadhabiburohman83@gmail.com>
2026-05-25 12:52:21 +0700

    au ah

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // 1. Redirect the empty path (root) to the register page
+  {
+    path: '',
+    redirectTo: 'register',
+    pathMatch: 'full'
+  },
+  // 2. Load the Register Module
+  {
+    path: 'register',
+    loadChildren: () => import('./features/register/register-module').then(m => m.RegisterModule)
+  },
+  // 3. Load the Order Module
+  {
+    path: 'order',
+    loadChildren: () => import('./features/order/order-module').then(m => m.OrderModule)
+  }
+];
