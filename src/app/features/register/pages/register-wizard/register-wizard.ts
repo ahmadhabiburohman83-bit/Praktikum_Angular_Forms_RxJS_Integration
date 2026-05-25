fc04bff44bb85c24bb4c5d935ebfbcf6bc2f6930
Ahmad Habiburrahman <ahmadhabiburohman83@gmail.com>
2026-05-25 11:23:08 +0700

    au ah

--- src/app/features/register/pages/register-wizard/register-wizard.ts
+++ src/app/features/register/pages/register-wizard/register-wizard.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+// 1. Import modul yang dibutuhkan
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-register-wizard',
-  imports: [],
-  templateUrl: './register-wizard.html',
-  styleUrl: './register-wizard.scss',
+  standalone: true,
+  // 2. Daftarkan di sini agar bisa digunakan di HTML-nya
+  imports: [
+    ReactiveFormsModule,
+    MatStepperModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './register-wizard.html', // Sesuaikan dengan path file HTML Anda
+  styleUrls: [] 
 })
-export class RegisterWizard {}
+export class RegisterWizardComponent {
+  // Logika komponen Anda
+}
