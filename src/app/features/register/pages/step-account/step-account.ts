d39d765b25a288afee43aa57e34ff7fe03c4d6da
Ahmad Habiburrahman <ahmadhabiburohman83@gmail.com>
2026-05-25 13:33:00 +0700

    akhirrnyaaaa

--- src/app/features/register/pages/step-account/step-account.ts
+++ src/app/features/register/pages/step-account/step-account.ts
@@ -1,12 +1,9 @@
-// src/app/features/register/pages/step-account/step-account.ts
-
-import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Component, Input, inject } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { UsernameAsyncValidator } from '../../../../core/validators/username-async'; // Sesuaikan path-nya
 
 @Component({
   selector: 'app-step-account',
@@ -21,18 +18,5 @@ import { UsernameAsyncValidator } from '../../../../core/validators/username-asy
   templateUrl: './step-account.html'
 })
 export class StepAccountComponent {
-  private fb = inject(FormBuilder);
-  
-  // Memanggil service yang sudah Anda buat
-  private usernameAsync = inject(UsernameAsyncValidator);
-
-  accountForm = this.fb.group({
-    username: [
-      '', 
-      {
-        validators: [], // Validator sinkron taruh di sini
-        asyncValidators: [this.usernameAsync.checkAvailability()] // Async validator taruh di sini
-      }
-    ]
-  });
+  @Input() form!: FormGroup;
 }
