444eb8ded264bf1a81fb7b390168cb949747f72d
Ahmad Habiburrahman <ahmadhabiburohman83@gmail.com>
2026-05-25 13:23:46 +0700

    asdasd

--- src/app/features/register/register-routing-module.ts
+++ src/app/features/register/register-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterWizardComponent } from './pages/register-wizard/register-wizard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RegisterWizardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
