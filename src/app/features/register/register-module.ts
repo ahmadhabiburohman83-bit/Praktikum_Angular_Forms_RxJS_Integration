e36fb3bf67de50be194cd05344bfa6bf39936704
Ahmad Habiburrahman <ahmadhabiburohman83@gmail.com>
2026-06-01 07:10:45 +0700

    fix registes wizaard

--- src/app/features/register/register-module.ts
+++ src/app/features/register/register-module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing-module';
+import { RegisterWizardComponent } from './pages/register-wizard/register-wizard';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, RegisterRoutingModule],
+  imports: [CommonModule, 
+    RegisterRoutingModule,
+    RegisterWizardComponent
+  ],
 })
 export class RegisterModule {}
